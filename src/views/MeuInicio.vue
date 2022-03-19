1de02a0d1808950dbcf76427af67876a865fe619
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-03-19 15:17:36 -0300

    PáginaInicial

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="meu-inicio">
-        
-    </div>
+    <v-container>
+    <div class="Meu-Inicio"></div>
+        <v-img
+        class= 'imagem-inicial'
+        :src="require('../assets/pascoa.jpg')">
+        </v-img>
+        <p class="text-uppercase mb-4 mt-6 font-weight-bold">
+        Conectando Pessoas!
+        </p>
+        <p class="text-justify mb-4 mt-6">
+            Aqui você encontra variadas opções de ovos de páscoa e lojistas tornando o consumo de ovos de páscoa mais democrático e feliz! 
+        </p>
+</v-container>
+    
 </template>
 
-
 <script>
-
-
 export default {
-    name: 'MeuInicio',
+    name: "MeuInicio"
 }
 </script>
 
-
 <style scoped>
+
 
 </style>
