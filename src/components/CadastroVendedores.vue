1c9b49ad195d9591dcea221dee43637e5d5e894a
Felipe Cardoso Duarte <lipecd91@gmail.com>
2022-03-19 14:23:13 -0300

    botoes cadastro

--- src/components/CadastroVendedores.vue
+++ src/components/CadastroVendedores.vue
@@ -1,39 +1,76 @@
 <template>
-    <div>
-        <h2>Cadastro de Vendedores</h2>
-
-        <p>Nome da Empresa</p>
-        <v-text-field
-        label="Nome"
-        outlined
-        ></v-text-field>
-
-        <p>CEP</p>
-        <v-text-field
+    <v-container>
+      <v-card-title primary-title class=" title mb-4">
+        Cadastro de Vendedores
+      </v-card-title>
+    <form ref=form>
+      <v-text-field
+        v-model="Nome"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Nome da empresa"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      
+      <v-text-field
+        v-model="Cep"
+        :error-messages="emailErrors"
         label="CEP"
-        outlined
-        ></v-text-field>
-
-        <p>Endereço</p>
-        <v-text-field
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      
+      <v-text-field
+        v-model="Endereco"
+        :error-messages="nameErrors"
+        :counter="10"
         label="Endereço"
-        outlined
-        ></v-text-field>
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      
+      <v-text-field
+        v-model="Foto"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Foto do Avatar (insira uma url)"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>      
 
-        <p>Foto do Avatar (insira uma url)</p>
-        <v-text-field
-        label="Foto"
-        outlined
-        ></v-text-field>
-    </div>
+      <v-btn class="mr-4" color="red darken-1" @click="submit"> Cadastrar </v-btn>
+      <v-btn @click="reset"> Limpar </v-btn>
+    </form>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+           items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ], 
+        }
+    },
+    methods: {
+        reset () {
+        this.$refs.form.reset()
+      },
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .title {
+        color: #E53935!important;
+    }
 </style>
