410bc71bc6eedce1962e154a178ea880d6e6e283
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-03-19 11:47:02 -0300

    Adicionando Navegation

--- src/components/AppTopBar.vue
+++ src/components/AppTopBar.vue
@@ -12,6 +12,40 @@
 
       <v-spacer></v-spacer>
 
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+        >
+        <v-list
+            nav
+            dense
+        >
+            <v-list-item-group
+            v-model="group"
+            active-class="green lighten-1"
+            >
+            <router-link to='/'>
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-whistle</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+            </router-link>
+
+            <router-link to='/classificacao'>
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-soccer-field</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Classificação</v-list-item-title>
+            </v-list-item>
+            </router-link>
+            </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
       <v-btn icon>
         <v-icon>mdi-export</v-icon>
       </v-btn>
