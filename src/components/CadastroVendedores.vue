ad4718e4deae80ba3d0eb915aabf59fbd67f357f
Felipe Cardoso Duarte <lipecd91@gmail.com>
2022-03-19 12:09:57 -0300

    cadastro vendedores

---
+++ src/components/CadastroVendedores.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>Cadastro de Vendedores</h2>
+
+        <p>Nome da Empresa</p>
+        <v-text-field
+        label="Nome"
+        outlined
+        ></v-text-field>
+
+        <p>CEP</p>
+        <v-text-field
+        label="CEP"
+        outlined
+        ></v-text-field>
+
+        <p>Endereço</p>
+        <v-text-field
+        label="Endereço"
+        outlined
+        ></v-text-field>
+
+        <p>Foto do Avatar (insira uma url)</p>
+        <v-text-field
+        label="Foto"
+        outlined
+        ></v-text-field>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
