103e511224ad315e88cd106a651431f64b70f5f8
luizgnclvs <luizgnclvss@gmail.com>
2022-03-19 10:58:36 -0300

    adicionado ListaOvos

---
+++ src/views/ListaOvos.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">Ovos Caseiros</h2>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'ListaOvos',
+        data () {
+            return {
+                listaOvos: []
+            }
+        },
+        created () {
+            fetch('https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json')
+                .then(response => response.json())
+                .then(json => {
+                    this.listaOvos = json;
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
