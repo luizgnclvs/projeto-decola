f1d458c56d935d07c1407ed7a6ac4af8e8d12da8
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-03-19 15:50:09 -0300

    Incones e Cores

--- src/components/AppTopBar.vue
+++ src/components/AppTopBar.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="app-top-bar">
-    <v-app-bar app elevation="4" color="red darken-1" class="title-camp cor">
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title> Pascoa Inclusiva </v-toolbar-title>
+    <v-app-bar app elevation="4" color="brown darken-1" class="title-camp cor">
+      <v-app-bar-nav-icon color="white" @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title class="cor"> Pascoa Inclusiva </v-toolbar-title>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
-        <v-list-item-group v-model="group" active-class="red lighten-2 text--accent-4">
+        <v-list-item-group v-model="group" active-class="brown lighten-3 text--accent-4">
           <router-link to="/">
             <v-list-item>
               <v-list-item-icon>
@@ -18,7 +18,7 @@
           <router-link to="/sobreprojeto">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
+                <v-icon>mdi-information</v-icon>
               </v-list-item-icon>
               <v-list-item-title>Sobre</v-list-item-title>
             </v-list-item>
@@ -26,7 +26,7 @@
           <router-link to="/cadastroprodutos">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
+                <v-icon>mdi-cash-register</v-icon>
               </v-list-item-icon>
               <v-list-item-title>Cadastro Produtos</v-list-item-title>
             </v-list-item>
@@ -34,7 +34,7 @@
           <router-link to="/cadastrovendedores">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
+                <v-icon>mdi-account-plus</v-icon>
               </v-list-item-icon>
               <v-list-item-title>Cadastro Vendedores</v-list-item-title>
             </v-list-item>
@@ -42,7 +42,7 @@
           <router-link to="/listaovos">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
+                <v-icon>mdi-egg-easter</v-icon>
               </v-list-item-icon>
               <v-list-item-title>Lista Ovos</v-list-item-title>
             </v-list-item>
@@ -73,6 +73,9 @@ export default {
 }
 .app-top-bar a {
   text-decoration: none;
-  color:black;
+  color: black;
 }
+
+.cor{font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: 20px; color:blanchedalmond}
 </style>
