ead2b25d9dc164aec61a881a6890a63982bfa10f
Paula Cabral <art.paulac@gmail.com>
2022-03-19 11:15:14 -0300

    rotas

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
+  {
+    path: '/CadastroProduto',
+    name: 'Cadastro',
+    component: CadastroProduto
+  }
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  }
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  }
 ]
 
 const router = new VueRouter({
