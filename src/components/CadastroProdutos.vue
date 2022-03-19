29e9c9a19d5cb6a76e3c913507dba4071f87def1
Talis Aparecido dos Reis <talisapreis@gmail.com>
2022-03-19 15:59:08 -0300

    Correções Cadastro de Produtos

--- src/components/CadastroProdutos.vue
+++ src/components/CadastroProdutos.vue
@@ -1,55 +1,124 @@
 <template>
   <v-container>
-      <v-card-title primary-title>
-          Cadastro Produtos
-      </v-card-title>
+    <v-card-title primary-title> Cadastro Produtos </v-card-title>
     <form>
       <v-text-field
-        v-model="Produto"
-        :error-messages="nameErrors"
-        :counter="10"
-        label="Name"
+        v-model="nome"
+        :counter="100"
+        label="Nome"
         required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
       ></v-text-field>
       <v-text-field
-        v-model="Preco"
-        :error-messages="emailErrors"
+        v-model="preco"
         label="Preco"
+        prefix="R$"
         required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
       ></v-text-field>
       <v-text-field
-        v-model="Sabor"
-        :error-messages="nameErrors"
-        :counter="10"
+        v-model="sabor"
+        :counter="100"
         label="Sabor"
         required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
       ></v-text-field>
-      <v-checkbox
-        v-model="checkbox"
-        :error-messages="checkboxErrors"
-        label="Do you agree?"
-        required
-        @change="$v.checkbox.$touch()"
-        @blur="$v.checkbox.$touch()"
-      ></v-checkbox>
+      <v-file-input accept="image/*" label="Imagem"></v-file-input>
 
       <v-btn class="mr-4" @click="submit"> submit </v-btn>
       <v-btn @click="clear"> clear </v-btn>
+      <v-dialog v-model="dialog" max-width="290">
+        <v-card>
+          <v-card-title class="text-h5"> Cadastro de Produto </v-card-title>
+
+          <v-card-text>
+            Desculpe! Algum campo não foi presenchido corretamente!
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              OK
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialogSucess" hide-overlay persistent width="300">
+        <v-card color="primary" dark>
+          <v-card-text>
+            Cadastrando Produto
+            <v-progress-linear
+              indeterminate
+              color="white"
+              class="mb-0"
+            ></v-progress-linear>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+      <v-alert v-if="sucesso" color="green" dismissible type="success">Produto cadastrado com Sucesso!</v-alert>
     </form>
   </v-container>
 </template>
 
 <script>
 export default {
-    name: 'CadastroProdutos',
-    methods: {
-        
-    }
-}
+  name: "CadastroProdutos",
+  data: () => ({
+    nome: "",
+    preco: 0,
+    sabor: "",
+    dialog: false,
+    dialogSucess: false,
+    sucesso: false
+  }),
+  watch: {
+    dialogSucess(val) {
+      if (!val) return;
+
+      setTimeout(() => (this.dialogSucess = false), 4000);
+      setTimeout(() => (this.sucesso = true), 4000);
+      
+    },
+  },
+  methods: {
+    submit() {
+      const storageinfos = [];
+      const erros = [];
+
+      if (this.nome != "") {
+        storageinfos.push(this.nome);
+      } else {
+        erros.push("Produto must be at most 10 characters long");
+        erros.push("Produto is required.");
+        console.log(erros);
+      }
+
+      if (this.preco != "") {
+        storageinfos.push(this.preco);
+      } else {
+        erros.push("Preco must be at most 10 characters long");
+        erros.push("Preco is required.");
+        console.log(erros);
+      }
+
+      if (this.sabor != "") {
+        storageinfos.push(this.sabor);
+      } else {
+        erros.push("Sabor must be at most 10 characters long");
+        erros.push("Sabor is required.");
+        console.log(erros);
+      }
+
+      if (erros.length === 0) {
+        this.dialogSucess = true;
+        console.log(this.dialog);
+      } else {
+        this.dialog = true;
+      }
+      this.clear();
+    },
+    clear() {
+      this.nome = "";
+      this.preco = "";
+      this.sabor = "";
+    },
+  },
+};
 </script>
